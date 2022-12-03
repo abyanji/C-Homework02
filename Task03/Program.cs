052c102f61ee82ed83606be523bbc375fc45baa0
abyanji <a.byanjil99@gmail.com>
2022-12-03 20:20:52 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Напишите число от 1 до 7");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 6 || number == 7)
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
